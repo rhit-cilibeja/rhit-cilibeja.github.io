6cda952b836ca95c81a050f1abcf3ca33d3742ef
rhit-cilibeja <cilibeja@rose-hulman.edu>
2025-09-26 19:37:14 -0400

    Improved header and footer

--- resources/index.js
+++ resources/index.js
@@ -5,10 +5,12 @@ window.addEventListener("load", (e) => {
 
 function setupHeader() {
 	let header = document.querySelector("header");
+	header.className = "navTools";
 	header.innerHTML = "<nav><a href = 'index.html'>Home</a><a href = 'resume.html'>Resume</a><a href = 'portfolio.html'>Projects</a></nav>";
 }
 
 function setupFooter() {
 	let footer = document.querySelector("footer");
+	footer.className = "navTools";
 	footer.innerHTML = "<ul><li><a href = 'mailto:jacob.ciliberti@rhit.edu'>Email</a></li><li><a href = 'https://www.linkedin.com/in/jake-ciliberti'>Linkedin</a></li></ul>";
 }
