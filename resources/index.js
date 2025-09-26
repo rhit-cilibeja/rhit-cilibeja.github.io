c08ab259b598af682825524ef49e14ae448d2563
rhit-cilibeja <cilibeja@rose-hulman.edu>
2025-09-26 17:16:23 -0400

    Added outline for headers and footers

---
+++ resources/index.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", (e) => {
+	setupHeader()
+	setupFooter()
+})
+
+function setupHeader() {
+	let header = document.querySelector("header")
+}
+
+function setupFooter() {
+	let footer = document.querySelector("footer")
+}
